e248d282502a65ea568043f140536886796f80fb
Birendra Rokaha <birenrokaha1@gmail.com>
2020-09-06 02:12:36 +1000

    django F1 ranking

--- django/testProject/testProject/polls/views.py
+++ django/testProject/testProject/polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import get_list_or_404, get_object_or_404
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Question,Choice
 
@@ -16,7 +18,7 @@ def detail(request, question_id):
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question doesnot exist")
-    return render(request, 'polls/results.html', {'question': question})
+    return render(request, 'polls/detail.html', {'question': question})
 
 #  Get question adn display result
 
@@ -24,3 +26,20 @@ def results(request, question_id):
     question = get_object_or_404(Question, pk = question_id)
     return render(request, 'polls/results.html', {'question': question})
 
+# vote for a question
+def vote(request, question_id):
+    # print(request.POST['choice])
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk =request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # render the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question':question,
+            'error_message': "You havent' selected a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # always return HttpResponseRedirect
+        return HttpResponseRedirect(reverse('polls:results', args= (question.id,)))
