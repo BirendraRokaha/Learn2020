f0a768459b53b08fc29b051af06dcfaebcda1c7c
Birendra Rokaha <birenrokaha1@gmail.com>
2020-09-05 23:48:20 +1000

    render the lists

--- django/testProject/testProject/polls/admin.py
+++ django/testProject/testProject/polls/admin.py
@@ -2,4 +2,22 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin
+admin.site.site_header = "F1 Polls"
+admin.site.site_title = "F1 Polls Admin"
+admin.site.index_title = "F1 Polls - Admin Page"
+
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None,{'fields' : ['question_text']}),
+    ('Date Information', {'fields' : ['pub_date'], 'classes': ['collapse']}),]
+    inlines = [ChoiceInLine]
+
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
+
+admin.site.register(Question, QuestionAdmin)
