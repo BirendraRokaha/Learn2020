8e0b40d2d0b1fce926a84377700be33293e42102
Birendra Rokaha <birenrokaha1@gmail.com>
2020-09-07 23:53:32 +1000

    admin pages

--- django/TwoNodes(WEB APP)/TwoNodes/blog/views.py
+++ django/TwoNodes(WEB APP)/TwoNodes/blog/views.py
@@ -14,6 +14,27 @@ posts = [
         'title':'Second post',
         'content':'Two nodes is the next TwoNodes',
         'datePosted':'10 September 2020'
+    },
+        {
+        'author':'John Smith',
+        'title':'First post',
+        'content':'Good to have account with TwoNodes',
+        'datePosted':'9 September 2020'
+    },    {
+        'author':'John Smith',
+        'title':'First post',
+        'content':'Good to have account with TwoNodes',
+        'datePosted':'9 September 2020'
+    },    {
+        'author':'John Smith',
+        'title':'First post',
+        'content':'Good to have account with TwoNodes',
+        'datePosted':'9 September 2020'
+    },    {
+        'author':'John Smith',
+        'title':'First post',
+        'content':'Good to have account with TwoNodes',
+        'datePosted':'9 September 2020'
     }
 ]
 
