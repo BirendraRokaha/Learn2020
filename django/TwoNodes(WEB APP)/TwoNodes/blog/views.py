4cebc1ba271d73188f8c71e6b86fa7039c052006
Birendra Rokaha <birenrokaha1@gmail.com>
2020-09-07 21:35:52 +1000

    understanding templates

--- django/TwoNodes(WEB APP)/TwoNodes/blog/views.py
+++ django/TwoNodes(WEB APP)/TwoNodes/blog/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse  no longer needed
+
+
+posts = [
+    {
+        'author':'John Smith',
+        'title':'First post',
+        'content':'Good to have account with TwoNodes',
+        'DatePosted':'9 September 2020'
+    },
+    {
+        'author':'Johan Appleased',
+        'title':'Second post',
+        'content':'Two nodes is the next TwoNodes',
+        'DatePosted':'10 September 2020'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Welcome to TwoNodes.</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>About.</h1>')
+    return render(request, 'blog/about.html',{'title':'About Us.'})
+
+# def contact(request):
+#     return HttpResponse('<h1 style="color: 	#B8860B; text-align: center;">Contact us.</h1>')
+
