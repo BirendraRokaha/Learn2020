ce49131d1101b0824e4af05938bcf1de7f0f7b9c
Birendra Rokaha <birenrokaha1@gmail.com>
2020-09-07 17:56:52 +1000

    Unserstanding routhing and urls.py

--- django/TwoNodes(WEB APP)/TwoNodes/TwoNodes/urls.py
+++ django/TwoNodes(WEB APP)/TwoNodes/TwoNodes/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # If you want to use the www.sdfsdf.com/blog as the blog page
+    # path('blog/', include('blog.urls')),
+    path('', include('blog.urls')),
 ]
